c73e753ce29d8f411537525e64a93c770feb0cb5
Yuba Raj Khadka <ubrajkhadka@gmail.com>
2020-12-29 20:00:45 +1100

    Updates

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -1,5 +1,5 @@
-import { Message, Stan } from 'node-nats-streaming';
-import { Subjects } from './subjects';
+import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
 
 interface Event {
   subject: Subjects;
@@ -7,11 +7,11 @@ interface Event {
 }
 
 export abstract class Listener<T extends Event> {
-  abstract subject: T['subject'];
+  abstract subject: T["subject"];
   abstract queGroupName: string;
-  abstract onMessage(data: T['data'], msg: Message): void;
+  abstract onMessage(data: T["data"], msg: Message): void;
 
-  private client: Stan;
+  protected client: Stan;
   protected ackWait = 5 * 1000;
 
   constructor(client: Stan) {
@@ -34,7 +34,7 @@ export abstract class Listener<T extends Event> {
       this.subscriptionOptions()
     );
 
-    subscription.on('message', (msg: Message) => {
+    subscription.on("message", (msg: Message) => {
       console.log(`Message received: ${this.subject} / ${this.queGroupName}`);
 
       const parsedData = this.parseMessage(msg);
@@ -44,8 +44,8 @@ export abstract class Listener<T extends Event> {
 
   parseMessage(msg: Message) {
     const data = msg.getData();
-    return typeof data === 'string'
+    return typeof data === "string"
       ? JSON.parse(data)
-      : JSON.parse(data.toString('utf8'));
+      : JSON.parse(data.toString("utf8"));
   }
 }
